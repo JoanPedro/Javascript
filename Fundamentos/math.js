1f64b5d0852aa41efb5dcb27cfa1e5aed4001b00
JoanPedro <31076821+JoanPedro@users.noreply.github.com>
2020-03-01 10:49:08 -0300

    Atualiza Math e básicos
    Pequena modificação de "Math.pow" para "Math.pow()"

--- Fundamentos/math.js
+++ Fundamentos/math.js
@@ -4,4 +4,4 @@ const area = Math.PI * Math.pow(raio, 2) // Pi*r²
 console.log(area, typeof Math) // Math não é função, é Objeto.
 
 Math.PI // Constante do Objeto
-Math.pow // Função do Objeto
+Math.pow() // Função do Objeto
