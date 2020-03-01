c8e76cc479650d290baf2a0e207678100ef57ae3
JoanPedro <31076821+JoanPedro@users.noreply.github.com>
2020-03-01 08:23:05 -0300

    Adiciona Math e básicos

---
+++ Fundamentos/math.js
@@ -0,0 +1,7 @@
+const raio = 7
+const area = Math.PI * Math.pow(raio, 2) // Pi*r²
+
+console.log(area, typeof Math) // Math não é função, é Objeto.
+
+Math.PI // Constante do Objeto
+Math.pow // Função do Objeto
