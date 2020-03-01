cfdfb5524c2bc7edce33b55047e014842413b125
JoanPedro <31076821+JoanPedro@users.noreply.github.com>
2020-03-01 08:20:11 -0300

    Adiciona Cuidados com operações numéricas

---
+++ Fundamentos/cuidadosNumeros.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) // Infinito
+console.log("10" / 2) // Interpreta como número. Verifica dentro da string se o valor pode ser calculado.
+console.log("Ver" * 5)
+console.log(0.1 + 0.7) // Imprecisão com Float. Especificação o IEEE
+    //console.log(10.toString())
+console.log((10.32123).toFixed(2))
