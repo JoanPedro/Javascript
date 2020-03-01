cc767fede0a5805b43b73bd03985520f733ada4c
JoanPedro <31076821+JoanPedro@users.noreply.github.com>
2020-03-01 11:53:25 -0300

    Adiciona Atribuições

---
+++ Fundamentos/Intermediario/atribuicao.js
@@ -0,0 +1,13 @@
+const a = 10
+let b = 20
+
+b += a // b = b + a
+console.log(b)
+b -= a // b = b - a
+console.log(b)
+b *= 2 // b = b*2
+console.log(b)
+b /= 2 // b = b/2
+console.log(b)
+b %= 2 // b = b % 2
+console.log(b)
