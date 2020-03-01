d870a31fef3b448398d200d6f53a245406c7ce24
JoanPedro <31076821+JoanPedro@users.noreply.github.com>
2020-03-01 20:28:46 -0300

    Adicionado Estrutura IF (2)

---
+++ EstruturaDeControle/estruturaIf2.js
@@ -0,0 +1,26 @@
+function testando(num) {
+    if (num > 7)
+        console.log(num)
+    console.log('Final')
+}
+
+testando(6)
+testando(8)
+
+function testando2(num) {
+    if (num > 7); {
+        console.log(num)
+    }
+}
+
+testando2(6)
+testando2(8)
+
+function testando3(num) {
+    if (num > 7) {
+        console.log(num)
+    }
+}
+
+testando3(6)
+testando3(8)
