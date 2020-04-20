6e93f684054d55ed8d62d57da9937735cd4d7238
Joan Pedro <jpos.senai@gmail.com>
2020-04-20 15:12:42 -0300

    Callback filter()

--- Fundamentos/Callbacks/callbacks.js
+++ Fundamentos/Callbacks/callbacks.js
@@ -1,3 +1,5 @@
+// Callback 1
+
 const fabricantes = ["Mercedes", "Audi", "BMW"]
 
 function imprimir(nome, indice) {
@@ -9,3 +11,27 @@ fabricantes.forEach(imprimir)
 fabricantes.forEach( fabricante => {
   console.log(fabricante)
 })
+
+// Callback 2
+
+const notas = [7.7, 7, 3.4, 5, 8, 10, 4.9, 6.9, 5.5]
+const notasBaixasSemCallback = []
+let notasBaixasComCallback = []
+
+// Sem callback
+
+for(let i in notas) {
+  if(notas[i] < 7) {
+    notasBaixasSemCallback.push(notas[i]) // Push para adicionar as notas menores que 7.
+  }
+}
+
+console.log(notasBaixasSemCallback)
+
+// Com callback
+
+notasBaixasComCallback = notas.filter(nota => {
+  return nota < 7 // Retorna booleano para filtrar notas menores que 7.
+})
+
+console.log(notasBaixasComCallback)
