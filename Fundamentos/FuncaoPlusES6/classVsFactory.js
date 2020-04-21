07109a262360546b345a0134a0e45d5eb36612bf
Joan Pedro <jpos.senai@gmail.com>
2020-04-21 11:56:20 -0300

    Class para Construtora

--- Fundamentos/FuncaoPlusES6/classVsFactory.js
+++ Fundamentos/FuncaoPlusES6/classVsFactory.js
@@ -1,3 +1,5 @@
+// Class ES6
+
 class Pessoa {
   constructor(nome){
     this.nome = nome
@@ -11,6 +13,8 @@ class Pessoa {
 const pessoa1 = new Pessoa('Joan')
 pessoa1.falar()
 
+// Factory
+
 const CriarPessoa = nome => {
   return {
     falar: () => console.log(`Meu nome é ${nome}`)
@@ -19,3 +23,16 @@ const CriarPessoa = nome => {
 
 const JoanPedro = CriarPessoa('Joan Pedro')
 JoanPedro.falar()
+
+// Construtora
+
+function Pessoas(nome){
+  this.nome = nome
+
+  this.falar = _ => {
+    console.log(`Meu nome é ${this.nome}`)
+  }
+}
+
+const JoanPedroOS = new Pessoas('Joan Pedro Oliveira de Souza')
+JoanPedroOS.falar()
