b4db0206b419ce8793dc0c4a453a3f183c550473
Joan Pedro <jpos.senai@gmail.com>
2020-04-22 12:34:35 -0300

    Desafio JSON e Map

--- Fundamentos/Array/arrayMap.js
+++ Fundamentos/Array/arrayMap.js
@@ -16,3 +16,18 @@ let converteReal = nums.map(soma10)
   .map(paraDinheiro)
 
 console.log(converteReal)
+
+// JSON para Map
+
+const carrinhoDeCompras = [
+  '{ "nome": "Borracha", "preco": 3.45 }',
+  '{ "nome": "Caderno", "preco": 13.99 }',
+  '{ "nome": "Kit de lápis", "preco": 41.50 }',
+  '{ "nome": "Caneta", "preco": 7.50 }',
+]
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultadoPreco = carrinhoDeCompras.map(paraObjeto).map(apenasPreco)
+console.log(resultadoPreco)
